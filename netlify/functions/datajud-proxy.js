d8a692d947b3c573e15965c716d47ee2878127e4
alexandrefreitass <xandy4321@hotmail.com>
2024-10-17 00:59:32 -0300

    fix CORS policy ao fazer deploy no netlify

---
+++ netlify/functions/datajud-proxy.js
@@ -0,0 +1,39 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const numeroProcesso = event.queryStringParameters.numeroProcesso;
+  const tribunalAlias = event.queryStringParameters.tribunalAlias;
+
+  const apiUrl = `https://api-publica.datajud.cnj.jus.br/${tribunalAlias}/_search`;
+  
+  const headers = {
+    'Authorization': `ApiKey cDZHYzlZa0JadVREZDJCendQbXY6SkJlTzNjLV9TRENyQk1RdnFKZGRQdw==`,
+    'Content-Type': 'application/json'
+  };
+
+  const body = {
+    query: {
+      match: {
+        numeroProcesso: numeroProcesso
+      }
+    }
+  };
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(body)
+    });
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Erro ao buscar o processo.' })
+    };
+  }
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://api-publica.datajud.cnj.jus.br'  // URL completa da API para produção
-  };
+  production: true,
+  apiUrl: '/api'  // Aponta para o proxy da função Lambda no Netlify
+};
