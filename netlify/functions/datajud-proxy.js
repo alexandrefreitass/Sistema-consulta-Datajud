d39ef8517933025cecb4260867e6d58af2b19f51
alexandrefreitass <xandy4321@hotmail.com>
2024-11-18 03:57:11 -0300

    correção

--- netlify/functions/datajud-proxy.js
+++ netlify/functions/datajud-proxy.js
@@ -1,11 +1,9 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'; // Import do fetch em ES Modules
 
-exports.handler = async function (event) {
-  const numeroProcesso = event.queryStringParameters.numeroProcesso;
-  const tribunalAlias = event.queryStringParameters.tribunalAlias;
+export async function handler(event, context) {
+  const { numeroProcesso, tribunalAlias } = JSON.parse(event.body); // Ajuste para extrair do `body`
 
   const apiUrl = `https://api-publica.datajud.cnj.jus.br/${tribunalAlias}/_search`;
-
   const headers = {
     'Authorization': `ApiKey cDZHYzlZa0JadVREZDJCendQbXY6SkJlTzNjLV9TRENyQk1RdnFKZGRQdw==`,
     'Content-Type': 'application/json',
@@ -26,21 +24,16 @@ exports.handler = async function (event) {
       body: JSON.stringify(body),
     });
 
-    if (!response.ok) {
-      throw new Error(`Erro na resposta da API: ${response.statusText}`);
-    }
-
     const data = await response.json();
-
     return {
       statusCode: 200,
       body: JSON.stringify(data),
     };
   } catch (error) {
-    console.error('Erro na função Lambda:', error);
+    console.error('Erro na função Lambda:', error.message);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
+      body: JSON.stringify({ error: 'Erro ao processar a requisição.' }),
     };
   }
-};
+}
